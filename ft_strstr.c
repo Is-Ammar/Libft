d782bb03e1a5ba3acdb1021c6adf12ea4dda033d
iammar1 <eammarpro@gmail.com>
2024-08-10 21:43:57 +0100

    cprrected error

--- ft_strstr.c
+++ ft_strstr.c
@@ -6,14 +6,14 @@
 /*   By: iammar <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/07 16:11:03 by iammar            #+#    #+#             */
-/*   Updated: 2024/08/07 16:45:10 by iammar           ###   ########.fr       */
+/*   Updated: 2024/08/10 21:43:13 by iammar           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*ft_strstr(const char *str, const char *to_find)
+char	*ft_strstr(char *str, char *to_find)
 {
-	const char	*tmp_str;
-	const char	*tmp_to_find;
+	 char	*tmp_str;
+	 char	*tmp_to_find;
 
 	if (!*to_find)
 		return (str);
