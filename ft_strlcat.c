80442abbe61610fe3db55c2a6ee6710ee0b1ff33
iammar1 <eammarpro@gmail.com>
2024-09-03 22:00:39 +0100

    .

---
+++ ft_strlcat.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iammar <iammar@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/03 20:58:46 by iammar            #+#    #+#             */
+/*   Updated: 2024/09/03 21:48:56 by iammar           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcat(char *dest, const char *src, size_t l)
+{
+	size_t	dest_len;
+	size_t	src_len;
+	size_t	i;
+
+	i = 0;
+	dest_len = ft_strlen(dest);
+	src_len = ft_strlen((char *)src);
+	if (l <= dest_len)
+		return (src_len + l);
+	while (src[i] && (dest_len + i < l - 1))
+	{
+		dest[dest_len + i] = src[i];
+		i++;
+	}
+	dest[dest_len + i] = '\0';
+	return (dest_len + src_len);
+}
