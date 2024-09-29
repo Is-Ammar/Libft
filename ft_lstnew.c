78ac857b5dd5f80751b6f8cc1a353b10de48516e
iammar1 <eammarpro@gmail.com>
2024-09-29 16:40:24 +0100

    add: bonus part

---
+++ ft_lstnew.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iammar <iammar@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/28 22:07:13 by iammar            #+#    #+#             */
+/*   Updated: 2024/09/29 16:39:35 by iammar           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void const *content, size_t content_size)
+{
+	t_list	*new_list;
+
+	new_list = (t_list *)malloc(sizeof(t_list));
+	if (!new_list)
+	{
+		return (NULL);
+	}
+	if (!content)
+	{
+		new_list->content = NULL;
+		new_list->content_size = 0;
+	}
+	else
+	{
+		new_list->content = malloc(content_size);
+		if (!new_list->content)
+		{
+			free(new_list);
+			return (NULL);
+		}
+		ft_memcpy(new_list->content, content, content_size);
+		new_list->content_size = content_size;
+	}
+	new_list->next = NULL;
+	return (new_list);
+}
